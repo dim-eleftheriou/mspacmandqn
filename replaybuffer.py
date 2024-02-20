190bb0794b71361c2d2b6e063ec54e9a39a9bb7e
dim-eleftheriou <dimelf93@gmail.com>
2024-02-20 16:59:40 +0200

    Device selection added
    Add device selection for getting batch of experiences to the selected device

--- replaybuffer.py
+++ replaybuffer.py
@@ -5,14 +5,16 @@ from random import shuffle
 class ExperienceReplay:
   """ExperienceReplay class is used for storing the trajectories over time.
   Arguments for instatiating the class:
-    N: Maximum number of trajectories stored in memory list. Default value is 500
-    batch_size: Number of trajectories to sample from memory list. Default value is 100
+    N (int): Maximum number of trajectories stored in memory list. Default value is 500
+    batch_size (int): Number of trajectories to sample from memory list. Default value is 100
+    DEVICE (str): Device to return batches of experiences for training
     """
-  def __init__(self, N:int=5000, batch_size:int=128):
+  def __init__(self, N:int=5000, batch_size:int=128, DEVICE='cuda:0'):
     self.N = N
     self.batch_size = batch_size
     self.memory = []
     self.counter = 0
+    self.DEVICE = DEVICE
 
   # Append memory list with experience tuples
   def add_memory(self, state1, action, reward, state2, terminated):
@@ -45,9 +47,9 @@ class ExperienceReplay:
     # Select randomly trajectories from memory
     ind = np.random.choice(np.arange(len(self.memory)), batch_size, replace=False)
     batch = [self.memory[i] for i in ind] #batch is a list of tuples (state1, action, reward, state2, terminated)
-    state1_batch = torch.stack([x[0].squeeze(dim=0) for x in batch], dim=0)
-    action_batch = torch.tensor([x[1] for x in batch]).long()
-    reward_batch = torch.Tensor([x[2] for x in batch])
-    state2_batch = torch.stack([x[3].squeeze(dim=0) for x in batch], dim=0)
-    terminated_batch = torch.Tensor([x[4] for x in batch])
+    state1_batch = torch.stack([x[0].squeeze(dim=0) for x in batch], dim=0).to(self.DEVICE)
+    action_batch = torch.tensor([x[1] for x in batch]).long().to(self.DEVICE)
+    reward_batch = torch.Tensor([x[2] for x in batch]).to(self.DEVICE)
+    state2_batch = torch.stack([x[3].squeeze(dim=0) for x in batch], dim=0).to(self.DEVICE)
+    terminated_batch = torch.Tensor([x[4] for x in batch]).to(self.DEVICE)
     return state1_batch, action_batch, reward_batch, state2_batch, terminated_batch
